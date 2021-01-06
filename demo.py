70eac3587c8b6b629501b2be90226ddd319bd944
AthaIoan <38070221+johnyath@users.noreply.github.com>
2021-01-06 01:06:26 +0100

    Add files via upload

--- demo.py
+++ demo.py
@@ -5,10 +5,13 @@ from torch.utils.data import TensorDataset, DataLoader
 import numpy as np
 from model import VAE
 
+
 args = SimpleNamespace(data_split=[0.7964376590330788, 0.2, 0.2],
                        batch_size=100,
                        shuffle=True,
                        epochs=1000,
+                       warm_up_epoch=100,
+                       lr=5*1e-4,
                        latent_length=40,
                        use_gpu=torch.cuda.is_available())
 
@@ -42,8 +45,6 @@ test_data = dataset[test_idx]
 test_data = torch.Tensor(test_data)
 
 
-
-
 vae = VAE(args.data_length, args.latent_length)
 if args.use_gpu:
     vae.cuda()
@@ -55,9 +56,24 @@ else:
     val_loader = DataLoader(val_data, batch_size=args.batch_size, shuffle=args.shuffle)
     test_loader = DataLoader(test_data, batch_size=args.batch_size, shuffle=args.shuffle)
 
-## train model
-vae.train(train_loader, args.epochs, args.batch_size)
+## initializing optimizer
+optimizer = AdamNormGrad(vae.parameters(), lr=args.lr)
+optimizer.zero_grad()
 
+for epoch in range(args.epochs):
+
+    ## annealing the KL_coef
+    KL_coef = (epoch + 1) / args.warm_up_epoch  ## KL_coef: contribution of prior distribution
+    ## done warm upping
+    if epoch > args.warm_up_epoch:
+        KL_coef = 1
+
+    ## train model
+    train_loss, train_R_loss, train_KL_loss = vae.train_model(train_loader, optimizer, KL_coef)
+
+    ## val model
+    val_loss, val_R_loss, val_KL_loss = vae.val_model(val_loader)
+    print("")
 
 print("")
 
--- model.py
+++ model.py
@@ -81,6 +81,7 @@ class VAE(nn.Module):
 
         ## output-distributions
         output_mean = self.decoder_output_mean(x)
+        output_mean = self.sigmoid(output_mean) ## 0-1 scale (why not softmax)??
 
         ## clamping for FrayFaces ##
         output_mean = torch.clamp(output_mean, min=0. + 1. / 512., max=1. - 1. / 512.) ## output ranging from 0.5/256 - (1-0.5/256)
@@ -117,15 +118,84 @@ class VAE(nn.Module):
 
         return output_mean, output_sigma, z_prior, latent_mean, latent_sigma
 
-    def train(self, data_loader, epochs, batch_size):
+    def train_model(self, data_loader, optimizer, KL_coef=1):
+
+        self.train()
 
         for index, data_batch in enumerate(data_loader):
 
-            self.forward(data_batch)
-
+            ## vizualize input
             # plt.imshow(data_batch.data.cpu().numpy()[0].reshape(28,20))
             # plt.show()
 
-        print("")
+            output_mean, output_sigma, z_prior, latent_mean, latent_sigma = self.forward(data_batch)
+            ### computing ELBO-loss
+            ## KL divergence
+            log_z_prior = torch.sum(-0.5 * torch.pow(z_prior, 2), 1) ## summing over the
+            log_q_z = torch.sum(-0.5 * (latent_sigma + torch.pow(z_prior - latent_mean, 2) / torch.exp(latent_sigma)), 1)
+            KL_loss = - (log_z_prior - log_q_z) ## KL(q||p)=Sum_over( q(z)*log[q(z)/p(z)])
 
-        print("")
+            ### computing reconstruction loss
+            ## Considering a discrete logistic distribution for FrayFace dataset (similarly to the paper)
+            ## upper pixel of generated output
+            data_batch = torch.floor(data_batch * 256) / 256
+
+            ## applying the logistic distributions
+            var = torch.exp(output_sigma) ## extracting variance
+            logistic_CDF_left = 1 / (1 + torch.exp(- (data_batch - output_mean)/var))
+            logistic_CDF_right = 1 / (1 + torch.exp(- (data_batch - output_mean + 1/256)/var))
+
+            R_loss = torch.sum(- torch.log(logistic_CDF_right - logistic_CDF_left + 1e-7), dim=1)
+
+            ## Overall loss considering both image reconstruction and respect to prior z
+            loss = R_loss + KL_coef * KL_loss
+
+            ## average over the batch's samples
+            loss = torch.mean(loss)
+
+            loss.backward()
+
+            ## updating NN-weights
+            optimizer.step()
+
+            ## resetting optimizer
+            optimizer.zero_grad()
+
+        return loss, torch.mean(R_loss), torch.mean(KL_loss)
+
+    def val_model(self, data_loader, KL_coef=1):
+
+        self.eval()
+
+        for index, data_batch in enumerate(data_loader):
+
+            ## vizualize input
+            # plt.imshow(data_batch.data.cpu().numpy()[0].reshape(28,20))
+            # plt.show()
+
+            output_mean, output_sigma, z_prior, latent_mean, latent_sigma = self.forward(data_batch)
+            ### computing ELBO-loss
+            ## KL divergence
+            log_z_prior = torch.sum(-0.5 * torch.pow(z_prior, 2), 1) ## summing over the
+            log_q_z = torch.sum(-0.5 * (latent_sigma + torch.pow(z_prior - latent_mean, 2) / torch.exp(latent_sigma)), 1)
+            KL_loss = - (log_z_prior - log_q_z) ## KL(q||p)=Sum_over( q(z)*log[q(z)/p(z)])
+
+            ### computing reconstruction loss
+            ## Considering a discrete logistic distribution for FrayFace dataset (similarly to the paper)
+            ## upper pixel of generated output
+            data_batch = torch.floor(data_batch * 256) / 256
+
+            ## applying the logistic distributions
+            var = torch.exp(output_sigma) ## extracting variance
+            logistic_CDF_left = 1 / (1 + torch.exp(- (data_batch - output_mean)/var))
+            logistic_CDF_right = 1 / (1 + torch.exp(- (data_batch - output_mean + 1/256)/var))
+
+            R_loss = torch.sum(- torch.log(logistic_CDF_right - logistic_CDF_left + 1e-7), dim=1)
+
+            ## Overall loss considering both image reconstruction and respect to prior z
+            loss = R_loss + KL_coef * KL_loss
+
+            ## average over the batch's samples
+            loss = torch.mean(loss)
+
+        return loss, torch.mean(R_loss), torch.mean(KL_loss)
